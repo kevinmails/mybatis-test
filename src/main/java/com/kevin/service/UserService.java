03be7966d50b8c2a8fe449bfa8312ac47f974d90
kevin.chen <chenbin-it@bestpay.com.cn>
2023-06-20 10:39:45 +0800

    更新数据库，使用mysql数据库

--- src/main/java/com/kevin/service/UserService.java
+++ src/main/java/com/kevin/service/UserService.java
@@ -2,11 +2,17 @@ package com.kevin.service;
 
 import com.kevin.dao.User;
 import com.kevin.dao.UserDao;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 /**
  * @author kevin
  */
+@Slf4j
 @Service
 public class UserService {
 
@@ -21,7 +27,23 @@ public class UserService {
         return userDao.getUserById(id);
     }
 
+    /**
+     * 测试嵌套事务的传播属性
+     *
+     * @param user
+     * @return
+     */
+    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Throwable.class)
     public boolean addUser(User user) {
+
+        this.addUserTA(new User(12345, "kevin", "abc", new Date()));
+
+        return userDao.addUser(user);
+    }
+
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Throwable.class)
+    public boolean addUserTA(User user) {
         return userDao.addUser(user);
     }
 
---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,8 @@
+-- DROP TABLE IF EXISTS t_user;
+CREATE TABLE if not exists `t_user` (
+                          `id` int NOT NULL,
+                          `user_name` varchar(45) DEFAULT NULL,
+                          `order_no` varchar(45) DEFAULT NULL,
+                          `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
