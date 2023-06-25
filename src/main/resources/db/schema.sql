adf90fa88903b09386b22ca346aa93a73a85751c
kevin.chen <chenbin-it@bestpay.com.cn>
2023-06-25 10:52:23 +0800

    web容器换成undertow

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -5,4 +5,4 @@ CREATE TABLE if not exists `t_user` (
                           `order_no` varchar(45) DEFAULT NULL,
                           `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
                           PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
